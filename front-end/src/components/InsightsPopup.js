b1b0697d7bf6e9a61241acc3abfce0042403bd8a
jc <jeanclaudeabouelias@gmail.com>
2025-03-11 22:17:52 -0400

    Added Insights pop up

---
+++ front-end/src/components/InsightsPopup.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../css/TimelinePage.css';
+import ProgressChart from './ProgressChart'; // Import the chart component
+
+
+
+const InsightsPopup = ({ onClose }) => {
+  return (
+    <div className="insights-popup-overlay">
+      <div className="insights-popup-content">
+        <h2>Progress Overview</h2>
+
+        {/* Render the Progress Chart */}
+        <ProgressChart />
+
+        {/* Close Button */}
+        <button onClick={onClose} className="close-popup-button">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default InsightsPopup;
